7901d27f0dd7530d4983b30d451c437d6bdad3c4
Harsh Choksi <hhchoksi.976@gmail.com>
2022-12-31 15:42:32 +0530

    900

---
+++ CODEFORCES/Power_Consumption_Calculation.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define c cout<<
+int main(){
+    ll n,p1,p2,p3,t1,t2,res=0,ans=0;
+    cin>>n>>p1>>p2>>p3>>t1>>t2;
+    vector<ll> v;
+    for(ll i=0;i<2*n;i++){
+        int k;
+        cin>>k;
+        v.push_back(k);
+    }
+    for(ll i=0;i<2*n;i+=2){
+        if(i==((2*n)-2)){
+            res+=((v[i+1]-v[i])*p1);
+            
+        }
+        else{
+            res+=((v[i+1]-v[i])*p1);
+            c res<<endl;
+            ans = v[i+2]-v[i+1];
+            c ans<<endl;
+            if(ans>=t1){
+                res+=(t1*p1);
+                ans-=t1;
+                if(ans>=t2){
+                    res+=(t2*p2);
+                    ans-=t2;
+                    res+=(ans*p3);
+                }
+                else{
+                    res+=(ans*p2);
+                }
+            }
+            else{
+                res+=(ans*p1);
+            }
+            
+        }
+    }
+    c res<<endl;
+    return 0;
+}
