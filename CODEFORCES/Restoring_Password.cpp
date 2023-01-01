0cddeaad9c8c585cd91b8a359c7efa7d5bce7c09
Harsh Choksi <hhchoksi.976@gmail.com>
2023-01-02 05:01:10 +0530

    900

---
+++ CODEFORCES/Restoring_Password.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll string
+
+int main(){
+    int a=0;
+    map<ll,int> map;
+    ll n;
+    cin>>n;
+    for(int i=0;i<10;i++){
+       ll k;
+       cin>>k;
+       map[k]=i;
+    }
+    for(int i=0;i<8;i++){
+        ll b = n.substr(a,10);
+        map.find(b);
+        a+=10;
+        cout<<map[b];
+    }
+    return 0;
+}
